807048b19db04412fbbb8ebe1a2684df8b915209
codystack <ebuzzadvert@gmail.com>
2020-09-16 03:55:49 +0100

    Octrax V1

---
+++ clients.php
@@ -0,0 +1,21 @@
+<?php
+include ('./components/header.php');
+include ('./components/navbar.php');
+?>
+
+    <section class="hero-wrap hero-wrap-2 degree-right" style="background-image: url('https://i.imgur.com/N6hu4VU.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text js-fullheight align-items-end">
+				<div class="col-md-9 ftco-animate pb-5 mb-5">
+					<p class="breadcrumbs"><span class="mr-2"><a href="\">Home <i class="fa fa-chevron-right"></i></a></span> <span>Clients <i class="fa fa-chevron-right"></i></span></p>
+					<h1 class="mb-3 bread">Our Clients</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
+<?php include('./components/footer.php'); ?>
