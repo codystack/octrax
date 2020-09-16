1c1f6904ef9d9c4bbf09f9413ff6b33097ec6ecc
codystack <ebuzzadvert@gmail.com>
2020-09-16 12:06:37 +0100

    Octrax V1

---
+++ services.php
@@ -0,0 +1,126 @@
+<?php
+include ('./components/header.php');
+include ('./components/navbar.php');
+?>
+
+	<section class="hero-wrap hero-wrap-2 degree-right" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text js-fullheight align-items-end">
+				<div class="col-md-9 ftco-animate pb-5 mb-5">
+					<p class="breadcrumbs"><span class="mr-2"><a href="\">Home <i class="fa fa-chevron-right"></i></a></span> <span>services <i class="fa fa-chevron-right"></i></span></p>
+					<h1 class="mb-3 bread">Our Services</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-section ftco-portfolio">
+		<div class="row justify-content-center no-gutters">
+			<div class="col-md-12 heading-section text-center ftco-animate mb-5">
+				<h2 class="mb-2">Our Services</h2>
+				<p>With a combined team of experienced professionals, <br>we are able to deliver to our clients the best services in the following areas.</p>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row no-gutters pb-5">
+				<div class="col-md-12 portfolio-wrap mt-0">
+					<div class="row no-gutters align-items-center">
+						<div class="col-md-5 img" style="background-image: url(https://images.pexels.com/photos/3207531/pexels-photo-3207531.jpeg);">
+							
+						</div>
+						<div class="col-md-7">
+							<div class="text pt-5 pl-0 pl-lg-5 pl-md-4 ftco-animate">
+								<div class="px-4 px-lg-4">
+									<div class="desc">
+										<div class="top">
+											<h2 class="mb-2"><a href="oilfield-support-services">Oilfield Support Services</a></h2>
+										</div>
+										<div class="absolute">
+											<p>Octrax Nigeria Limited provides full logistics support in the field of Crane Hiring, Heavy Lifting, Transportation and Equipment rental services.</p>
+											<p><a href="oilfield-support-services" class="custom-btn">Read More</a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 portfolio-wrap">
+					<div class="row no-gutters align-items-center">
+						<div class="col-md-5 order-md-last img" style="background-image: url(https://coralshippinglogistics.com/wp-content/uploads/2020/02/air-freight-cargo-is-header.jpg);">
+							
+						</div>
+						<div class="col-md-7">
+							<div class="text pt-5 pr-md-5 ftco-animate">
+								<div class="px-4 px-lg-4">
+									<div class="desc text-md-right">
+										<div class="top">
+											<h2 class="mb-2"><a href="custom-brokerage-solutions">Custom Brokerage Solutions</a></h2>
+										</div>
+										<div class="absolute">
+											<p>We will work with you to ensure that shipments reach their destinations as planned. </p>
+											<p>We stay current on all the relevant import regulations and updates. We provide exceptional services to any and all types of industries.</p>
+											<p><a href="custom-brokerage-solutions" class="custom-btn">Red More</a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 portfolio-wrap mt-0">
+					<div class="row no-gutters align-items-center">
+						<div class="col-md-5 img" style="background-image: url(https://www.joc.com/sites/default/files/field_feature_image/Modes%2BSALrefineryWEB.jpg);">
+							
+						</div>
+						<div class="col-md-7">
+							<div class="text pt-5 pl-0 pl-lg-5 pl-md-4 ftco-animate">
+								<div class="px-4 px-lg-4">
+									<div class="desc">
+										<div class="top">
+											<h2 class="mb-2"><a href="project-logistics">Project Logistics/Rigmoves</a></h2>
+										</div>
+										<div class="absolute">
+											<p>We know that each project is unique and presents its own specific challenges.</p>
+											<p>Whether it`s a one of heavy lifting exercise or a long term construction development that requires sustained logistical support.</p>
+											<p><a href="project-logistics" class="custom-btn">Read More</a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 portfolio-wrap">
+					<div class="row no-gutters align-items-center">
+						<div class="col-md-5 order-md-last img" style="background-image: url(https://www.securitymagazine.com/ext/resources/SEC/2016/0416/1SEC0416-supply-feature-slide1_900px.jpg);">
+							
+						</div>
+						<div class="col-md-7">
+							<div class="text pt-5 pr-md-5 ftco-animate">
+								<div class="px-4 px-lg-4">
+									<div class="desc text-md-right">
+										<div class="top">
+											<h2 class="mb-2"><a href="procurement">Procurement/Supply Chain Solutions</a></h2>
+										</div>
+										<div class="absolute">
+											<p>We procure and supply high quality equipment, Spares and Machinery for the Oil and Gas, Energy, Construction industries etc.</p>
+											<p><a href="procurement" class="custom-btn">Red More</a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+<?php include('./components/footer.php'); ?>
