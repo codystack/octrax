e29375186a41830fc3f12ffe4f9d223c910aa45f
codystack <ebuzzadvert@gmail.com>
2020-09-16 13:58:07 +0100

    Octrax V1

---
+++ oilfield-support-services.php
@@ -0,0 +1,52 @@
+<?php
+include ('./components/header.php');
+include ('./components/navbar.php');
+?>
+
+	<section class="hero-wrap hero-wrap-2 degree-right" style="background-image: url('https://i.imgur.com/yDhEePU.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text js-fullheight align-items-end">
+				<div class="col-md-9 ftco-animate pb-5 mb-5">
+					<p class="breadcrumbs"><span class="mr-2"><a href="\">Home <i class="fa fa-chevron-right"></i></a></span> <span class="mr-2"><a href="services">Services <i class="fa fa-chevron-right"></i></a></span> <span>Oilfield Support Services <i class="fa fa-chevron-right"></i></span></p>
+					<h1 class="mb-3 bread">Oilfield Support Services</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-section ftco-portfolio">
+
+		<div class="container">
+			<div class="row no-gutters pb-5">
+				<div class="col-md-12 portfolio-wrap mt-0">
+					<div class="row no-gutters align-items-center">
+						<div class="col-md-5 img" style="background-image: url(https://images.pexels.com/photos/3207531/pexels-photo-3207531.jpeg);">
+							
+						</div>
+						<div class="col-md-7">
+							<div class="text pt-5 pl-0 pl-lg-5 pl-md-4 ftco-animate">
+								<div class="px-4 px-lg-4">
+									<div class="desc">
+										<div class="top">
+											<h2 class="mb-2"><a href="#">Oilfield Support Services</a></h2>
+										</div>
+										<div class="absolute">
+                                            <p>Octrax Nigeria Limited provides full logistics support in the field of Crane Hiring, Heavy Lifting, Transportation and Equipment rental services.</p>
+                                            <p>We are highly specialized in the supply of Mobile Cranes with various capacities ranging from 30tons to 300tons, Heavy and Transport equipment like Lowbeds and Flatbeds trailers, Truck heads, Wheel Loaders, Forklifts, Manlifts, Telehandlers, Air compressors, Welding machines, Power generators, and tower lights etc.</p>
+                                            <p>Our manpower consists of qualified and certified Crane Operators, Trailer Drivers, Heavy equipment Operators and Riggers.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+			</div>
+		</div>
+	</section>
+
+<?php include('./components/footer.php'); ?>
---
+++ procurement.php
@@ -0,0 +1,53 @@
+<?php
+include ('./components/header.php');
+include ('./components/navbar.php');
+?>
+
+	<section class="hero-wrap hero-wrap-2 degree-right" style="background-image: url('https://i.imgur.com/LJ7mkq5.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text js-fullheight align-items-end">
+				<div class="col-md-9 ftco-animate pb-5 mb-5">
+					<p class="breadcrumbs"><span class="mr-2"><a href="\">Home <i class="fa fa-chevron-right"></i></a></span> <span class="mr-2"><a href="services">Services <i class="fa fa-chevron-right"></i></a></span> <span>Procurement/Supply Chain Solutions <i class="fa fa-chevron-right"></i></span></p>
+					<h1 class="mb-3 bread">Procurement/Supply Chain Solutions</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-section ftco-portfolio">
+
+		<div class="container">
+			<div class="row no-gutters pb-5">
+				<div class="col-md-12 portfolio-wrap mt-0 mb-5">
+					<div class="row no-gutters align-items-center">
+						<div class="col-md-5 img" style="background-image: url(https://www.securitymagazine.com/ext/resources/SEC/2016/0416/1SEC0416-supply-feature-slide1_900px.jpg);">
+							
+						</div>
+						<div class="col-md-7">
+							<div class="text pt-5 pl-0 pl-lg-5 pl-md-4 ftco-animate">
+								<div class="px-4 px-lg-4">
+									<div class="desc">
+										<div class="top">
+											<h2 class="mb-2"><a href="#">Procurement/Supply Chain Solutions</a></h2>
+										</div>
+										<div class="absolute">
+                                            <p>We procure and supply high quality equipment, Spares and Machinery for the Oil and Gas, Energy, Construction industries etc. <strong>Oil and Gas</strong></p>
+                                            <p>Safety equipment (PPE), Rigging supplies, Marine supplies, Welding Supplies, Generators, Pumps and fitters, Pipes and Fittings, Gauge and Meters, Slings, Shackles, Lids, Lightings and Electrical equipment, NDT materials etc. <strong>Tools and Machineries</strong></p>
+                                            <p>Handtools, Machine tools, Pneumatic and Hydraulic, Tube cleansing and repair, Hydra blasters, Air conditioners etc. <strong>Chemicals</strong></p>
+                                            <p>Industrial chemicals, Drilling fluid, Acids, Caustics, Solvents, Process chemicals, Water treatment chemicals, Activated charcoals, Oilfield Corrosion, Inhibitors, Coatings, Resins, Paints etc.</p>
+                                            </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+			</div>
+		</div>
+	</section>
+
+<?php include('./components/footer.php'); ?>
