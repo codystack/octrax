531c674f95bd47137d95c981be92334a1336680f
codystack <ebuzzadvert@gmail.com>
2020-09-16 11:07:59 +0100

    Octrax V1

--- clients.php
+++ clients.php
@@ -3,7 +3,7 @@ include ('./components/header.php');
 include ('./components/navbar.php');
 ?>
 
-    <section class="hero-wrap hero-wrap-2 degree-right" style="background-image: url('https://i.imgur.com/N6hu4VU.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2 degree-right" style="background-image: url('https://i.imgur.com/ALt0ohR.jpg');" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row no-gutters slider-text js-fullheight align-items-end">
@@ -11,6 +11,41 @@ include ('./components/navbar.php');
 					<p class="breadcrumbs"><span class="mr-2"><a href="\">Home <i class="fa fa-chevron-right"></i></a></span> <span>Clients <i class="fa fa-chevron-right"></i></span></p>
 					<h1 class="mb-3 bread">Our Clients</h1>
 				</div>
+			</div>
+		</div>
+    </section>
+    
+    <section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center no-gutters">
+				<div class="col-md-12 heading-section text-center ftco-animate mb-5">
+                    <h2 class="mb-2">HERE ARE SOME BRANDS WE WORKED FOR.</h2>
+                    <p>The qualitative approach to our service cannot be over-emphasized. HEDO-BEC employs the 21st century compliant and cutting edge technology to deliver both in-the-site and out-of-site service leaving a long lasting impression of professionalism and excellence.</p>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+                
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/aSvbAHR.png">
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/UtjnUqh.png">
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/bNoXWFd.png">
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/nHV5vqJ.png">
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/BgXhDS2.jpg">
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/LyORi4t.png">
+                </div>
+                <div class="col-sm-12 col-md-4 col-lg-4 ftco-animate fadeInUp ftco-animated">
+                    <img src="https://i.imgur.com/Y46Npyw.jpg">
+                </div>
 			</div>
 		</div>
 	</section>
